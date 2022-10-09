a641d02f5bc80b8b65355e078194388add31ad4b
Ammoniaw <73943699+Ammoniaw@users.noreply.github.com>
2022-10-10 02:39:55 +0800

    Update 11-03 根据流程图编写代码.c

--- 小甲鱼C/fishC课后题/09-算数运算符/11-03 根据流程图编写代码.c
+++ 小甲鱼C/fishC课后题/09-算数运算符/11-03 根据流程图编写代码.c
@@ -1,15 +1,31 @@
 // 11-03 根据流程图编写代码
 #include <stdio.h>
+#include "ssebtr1.h"
 int main()
 {
-	int age, xin_lv;
+	int age, max_xin_lv, BPM, i;
 	printf("请您输入您的年龄：");
 	scanf("%d", &age);
-	xin_lv = 220 - age;
-	
-	
-	
-	
-	
+	max_xin_lv = 220 - age;
+	BPM = 150;
+	playSound(BPM);
+	i = getHeartRate();
+	if (i > max_xin_lv){
+		printf("请马上停止跑步，否则会有生命危险......\n");
+	}
+	else if (i > max_xin_lv * 0.85){
+		printf("请放慢脚步\n");
+		BPM -= 20;
+		playSound(BPM);
+	}
+	else if (i < max_xin_lv * 0.75 ){
+		printf("Come on, 请加快节奏！\n");
+		BPM += 20;
+		playSound(BPM);
+	}
+	else{
+		playSound(BPM)
+	}
+
 	return 0;
 }
